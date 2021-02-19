86f8d688d95638af1d189ae67e27debf37af5a10
Greg0109 <greg.rabago@gmail.com>
2021-02-19 12:07:58 +0100

    Add RSpec

---
+++ spec/meme_retriever_spec.rb
@@ -0,0 +1,18 @@
+require 'httparty'
+require 'nokogiri'
+require_relative '../lib/meme_retriever.rb'
+
+RSpec.describe MemeRetriever do
+    meme_retriever = MemeRetriever.new
+    describe "#get_url_content" do
+        it "Returns the content of the URL using the scrapper" do
+            expect(meme_retriever.get_url_content).to be_an_instance_of(Nokogiri::XML::Document)
+        end
+    end
+
+    describe "#get_memes" do
+        it "Returns an array of links of memes" do
+            expect(meme_retriever.get_memes).to be_an_instance_of(Array)
+        end
+    end
+end
