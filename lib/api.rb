243ad86429aa25f4ba4633c891049af56aca2524
Greg0109 <greg.rabago@gmail.com>
2021-02-19 11:28:19 +0100

    Update api.rb

--- lib/api.rb
+++ lib/api.rb
@@ -3,10 +3,10 @@ require 'twitter'
 class API
   def get_client
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key = ''
-      config.consumer_secret = ''
-      config.access_token = ''
-      config.access_token_secret = ''
+      config.consumer_key = 'API-Key'
+      config.consumer_secret = 'API-Key secret'
+      config.access_token = 'Access Token'
+      config.access_token_secret = 'Access Token Secret'
     end
     client
   end
